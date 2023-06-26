cc01fd3e89c76a753e2afcd595bfef25aa96b553
jainamshah0037 <jainamshah0037@gmail.com>
2023-06-26 01:30:15 -0400

    folder structure added and python notebook created

--- template.py
+++ template.py
@@ -20,7 +20,8 @@ list_of_files = [
     "config/config.yaml",
     "params.yaml",
     "app.py",
-    "main.py"
+    "main.py",
+    "tests.py",
     "Dockerfile",
     "requirements.txt",
     "setup.py",
